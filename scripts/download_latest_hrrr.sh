2b6dfd97b97fd25b27eb6068ad25a8befe0b039a
Alan Steremberg <alan.steremberg@gmail.com>
2015-10-29 11:45:35 -0700

    starting mrms scripts
    Former-commit-id: 36b8e7f810e0f17790022ea7e6f7d26a1e3a62c9

--- scripts/download_latest_hrrr.sh
+++ scripts/download_latest_hrrr.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 URL=http://nomads.ncep.noaa.gov/pub/data/nccf/nonoperational/com/hrrr/prod/
 DATE=`curl "$URL" | grep "href=\"h" | tail -n 1 | cut -f 2 -d \"`
---
+++ scripts/download_latest_mrms.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+URL=http://mrms.ncep.noaa.gov/data/2D/SeamlessHSR/
+echo $URL
+FILE=`curl $URL | grep "grib2.gz" |  tail -n 1 | cut -f 2 -d \"`
+echo $URL$FILE
+wget -N -c $URL$FILE
+
+echo "run command and pass $FILE "
+
+echo "create radar from $FILE "
+#./create_mrms_radar.sh  $FILE
+
