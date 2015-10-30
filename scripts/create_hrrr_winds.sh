7a8d647aeab4144e5bf03b66e5dcafdf5cf4cf9b
Alan Steremberg <alan.steremberg@gmail.com>
2015-10-29 17:59:37 -0700

    added windspeed - need to cleanup temp file
    Former-commit-id: beb573f1ce0cd43af3c36cbb870c1bf597f37924

--- scripts/create_hrrr_winds.sh
+++ scripts/create_hrrr_winds.sh
@@ -9,9 +9,15 @@ GRIBNAME=$1
 # this will print out all the variables in this file
 wgrib2 -v $GRIBNAME
 
+wgrib2 $GRIBNAME  -wind_speed WND.grib2
+
 #14:10481048:d=2015092203:UGRD:10 m above ground:15 min fcst:
 #15:11792425:d=2015092203:VGRD:10 m above ground:15 min fcst:
 
+gdaldem color-relief -b 2 WND.grib2 -alpha palettes/grey_uv.txt -of GTiff output/hrrr_speed_10.tiff
+python ./gdal2tiles.py -r bilinear  output/hrrr_speed_10.tiff output/hrrr_speed_tiles
+
+gdaldem color-relief -b 15 $GRIBNAME  -alpha palettes/grey_uv.txt -of GTiff output/hrrr_v_10.tiff
 gdaldem color-relief -b 14 $GRIBNAME  -alpha palettes/grey_uv.txt -of GTiff output/hrrr_u_10.tiff
 gdaldem color-relief -b 15 $GRIBNAME  -alpha palettes/grey_uv.txt -of GTiff output/hrrr_v_10.tiff
 python ./gdal2tiles.py -r bilinear  output/hrrr_u_10.tiff output/hrrr_u_tiles
