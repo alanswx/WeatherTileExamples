146df511e9e73338685b38eecf2fbfb44e6f04a8
Alan Steremberg <alan.steremberg@gmail.com>
2015-11-02 10:56:20 -0800

    added comments

--- scripts/create_mrms_tiles.sh
+++ scripts/create_mrms_tiles.sh
@@ -21,17 +21,28 @@ fi
 
 
 #
-# create the rain mask
+# create the rain and snow mask useing the precip type
 #
 
 gdaldem color-relief $file2  -alpha palettes/precip_rain_only.txt -of VRT rain_mask.vrt
 gdaldem color-relief $file2  -alpha palettes/precip_snow_only.txt -of VRT snow_mask.vrt
+
+#
+# create two images - one with the rain palette one with the snow palette (we will merge them later)
+#
 gdaldem color-relief  $file  -alpha palettes/radar_pal.txt -of VRT rain.vrt 
 gdaldem color-relief  $file  -alpha palettes/precipsnow_pal.txt -of VRT snow.vrt 
+
+#
+# pull only the alpha layer out of the mask (we could probably skip this step with a better palette?)
+#
 
 gdal_translate -b 4 snow_mask.vrt -of GTiff snow_mask.tif
 gdal_translate -b 4 rain_mask.vrt -of GTiff rain_mask.tif
 
+#
+# pull apart the individual bands, so we can restack them with a new alpha
+#
 gdal_translate -b 1 rain.vrt -of GTiff rainband1.tif
 gdal_translate -b 2 rain.vrt -of GTiff rainband2.tif
 gdal_translate -b 3 rain.vrt -of GTiff rainband3.tif
@@ -40,14 +51,20 @@ gdal_translate -b 1 snow.vrt -of GTiff snowband1.tif
 gdal_translate -b 2 snow.vrt -of GTiff snowband2.tif
 gdal_translate -b 3 snow.vrt -of GTiff snowband3.tif
 
+# create a new stack with the rain and snow mask as the alpha
+
 gdalbuildvrt -separate rainstack.vrt rainband1.tif rainband2.tif rainband3.tif rain_mask.tif
 gdalbuildvrt -separate snowstack.vrt snowband1.tif snowband2.tif snowband3.tif snow_mask.tif
 
+# not sure we need this step?
 gdal_translate snowstack.vrt -mask 4 -of GTiff snow.tif
 
+# overlay the snow on top of the rain
 gdalwarp rainstack.vrt snow.tif   -of GTiff rainsnow.tif
+# create tiles
 python ./gdal2tiles.py -r bilinear  rainsnow.tif $2 
 
+# remove temporary files
 rm rain.vrt
 rm snow.vrt
 rm rainband?.tif
