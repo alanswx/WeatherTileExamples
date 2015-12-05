218e91ceff5c18fe10917ab0b0d45a1cbc60921a
Alan Steremberg <alan.steremberg@gsa.gov>
2015-12-04 16:11:27 -0800

    added rain snow mask

--- scripts/create_hrrr_radar.sh
+++ scripts/create_hrrr_radar.sh
@@ -11,8 +11,14 @@ GRIBNAME=$1
 
 #83:80466128:d=2015102917:REFD Reflectivity [dB]:1000 m above ground:60 min fcst:
 
+#
+# use the snow mask to create a new.grib2 that is setup for the new palette
+#
+wgrib2 $GRIBNAME  -match ":(CSNOW|REFD):(surface|1000 m above ground):15 min"  -if ":CSNOW.*:.*:15.*" -rpn sto_1 -fi  -if ":REFD.*:1000 m.*:15.*:" -rpn sto_2 -fi -if_reg '1:2' -rpn "rcl_1:200:*:rcl_2:+"  -set_var REFD -grib_out new.grib2
 
-gdaldem color-relief -b 83 $GRIBNAME -alpha palettes/radar_pal.txt -of VRT output/hrrr.vrt
+
+#gdaldem color-relief -b 83 $GRIBNAME -alpha palettes/radar_pal.txt -of VRT output/hrrr.vrt
+gdaldem color-relief -b 83 new.grib2 -alpha palettes/radar_rainsnow_pal.txt -of VRT output/hrrr.vrt
 #python ./gdal2tiles.py -r bilinear  -z 0-9 output/hrrr.vrt output/hrrr
 python ./gdal2tiles.py -r bilinear  output/hrrr.vrt output/hrrr
 rm -f output/hrrr.vrt
